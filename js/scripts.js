0a904346be68dccc9c50c22b9ed9adc8627c6e5d
Samantha Lavin <salavin12@gmail.com>
2025-08-16 22:55:06 -0500

    hopefully some fixes for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,77 @@
-(async () => {
-  await loadSlim(tsParticles);
+const isMobile = /Mobi/.test(window.navigator.userAgent)
 
-  await tsParticles.load({
-    options: {
-        "particles": {
-            "number": {
-                "value": 30,
-                "density": {
-                    "enable": true,
-                    "value_area": 800
-                }
-            },
-            "shape": {
-                "type": "image",
-                "options": {
-                    "image": {
-                        "src": "https://samantha-lav.in/assets/leaf.png",
-                        "width": 100,
-                        "height": 100,
-                        "replaceColor": false
+if (!isMobile) {
+    (async () => {
+        await loadSlim(tsParticles);
+
+        await tsParticles.load({
+            options: {
+                "particles": {
+                    "number": {
+                        "value": 30,
+                        "density": {
+                            "enable": true,
+                            "value_area": 800
+                        }
+                    },
+                    "shape": {
+                        "type": "image",
+                        "options": {
+                            "image": {
+                                "src": "https://samantha-lav.in/assets/leaf.png",
+                                "width": 100,
+                                "height": 100,
+                                "replaceColor": false
+                            }
+                        }
+                    },
+                    "opacity": {
+                        "value": .8,
+                        "random": false,
+                    },
+                    "size": {
+                        "value": 20,
+                        "random": true,
+                    },
+                    "move": {
+                        "enable": true,
+                        "speed": 3,
+                        "direction": "bottom",
+                        "random": false,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                    },
+                    "rotate": {
+                        "direction": "random",
+                        "path": false,
+                        "value": {
+                            "min": 90,
+                            "max": 270
+                        },
+                        "animation": {
+                            "enable": true,
+                            "speed": 5,
+                            "sync": false
+                        }
+                    },
+                    "zIndex": {
+                        "value": {
+                            "min": 0,
+                            "max": 100
+                        },
+                        "opacityRate": 6,
+                        "sizeRate": 6
                     }
-                }
-            },
-            "opacity": {
-                "value": .8,
-                "random": false,
-            },
-            "size": {
-                "value": 20,
-                "random": true,
-            },
-            "move": {
-                "enable": true,
-                "speed": 3,
-                "direction": "bottom",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false,
-            },
-            "rotate": {
-                "direction": "random",
-                "path": false,
-                "value": {
-                    "min": 90,
-                    "max": 270
                 },
-                "animation": {
+                "fullscreen": {
                     "enable": true,
-                    "speed": 5,
-                    "sync": false
-                }
-            },
-            "zIndex": {
-                "value": {
-                    "min": 0,
-                    "max": 100
+                    "zIndex": -1000
                 },
-                "opacityRate": 6,
-                "sizeRate": 6
-            }
-        },
-        "fullscreen": {
-            "enable": true,
-            "zIndex": -1000
-        },
-    },
-  });
-})();
+            },
+        });
+        })();
+}
 
 const options = {
   top: '16px',
